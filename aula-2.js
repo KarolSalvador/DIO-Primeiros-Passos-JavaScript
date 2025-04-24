5d52da613bc4b1c6f3be5d8fb0dc4a9637d8326c
KarolSalvador <karolsalvador91@gmail.com>
2025-04-24 16:42:43 -0300

    criacao do arquivo aula-2, inserido verificacao de numero par

---
+++ aula-2.js
@@ -0,0 +1,9 @@
+//Estruturas condicionais
+//variáveis do tipo boolean irão receber verdadeiro ou falso (0 ou 1).
+
+const numero = 10;
+
+//para saber se um número é par, tenta dividir por 2 e ver o resto da divisão. Se sobrar resto não é par.
+const numeroPar = (numero % 2) === 0; //o % é utilizado para pegar o resto da divisão
+
+console.log(numeroPar)
