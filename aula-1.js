2e6860ab3e42d0141422b8b53e35e332f02b9b9b
KarolSalvador <karolsalvador91@gmail.com>
2025-04-24 16:33:49 -0300

    Alterado nome do arquivo aula.js e adicionado comentários

---
+++ aula-1.js
@@ -0,0 +1,8 @@
+//Variáveis e operadores
+//let é usado para declarar uma variável que poderá ser alterada
+let variavel = 10 * 10;
+
+//const é usado para declarar uma variável que não pode ser modificada, sendo ela uma constante
+const pi = 3.14;
+
+console.log(variavel);
