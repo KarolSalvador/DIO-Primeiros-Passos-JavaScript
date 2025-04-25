86e505c451fd439666b064a1ca48f8231e8c3c93
KarolSalvador <karolsalvador91@gmail.com>
2025-04-25 16:43:29 -0300

    criada condicional para verificar entrada

--- aula-2.js
+++ aula-2.js
@@ -1,9 +1,19 @@
 //Estruturas condicionais
 //variáveis do tipo boolean irão receber verdadeiro ou falso (0 ou 1).
 
-const numero = 10;
+const numero = 3;
 
 //para saber se um número é par, tenta dividir por 2 e ver o resto da divisão. Se sobrar resto não é par.
-const numeroPar = (numero % 2) === 0; //o % é utilizado para pegar o resto da divisão
+const numeroDivisivelPor5 = (numero % 5) === 0; //o % é utilizado para pegar o resto da divisão
 
-console.log(numeroPar)
+//if é uma forma de criar condicionais para direcionar o que irá acontecer de acordo com a condição
+if (numero === 0) {
+    console.log("Número inválido");
+
+} else if (numeroDivisivelPor5) { //usado para testar uma nova condição quando a anterior no if for falsa.
+    console.log("Sim");
+
+} else { //bloco para se não entrar na condição do if cair nessa
+    console.log("Não");
+}
+
