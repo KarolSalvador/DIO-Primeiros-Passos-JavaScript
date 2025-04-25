c69fe2752e3fedee0bf80f5c54b0e4189632edd7
KarolSalvador <karolsalvador91@gmail.com>
2025-04-25 16:45:59 -0300

    alteracao da variavel bool, criacao do arquivo desafio-2

--- aula-2.js
+++ aula-2.js
@@ -4,13 +4,13 @@
 const numero = 3;
 
 //para saber se um número é par, tenta dividir por 2 e ver o resto da divisão. Se sobrar resto não é par.
-const numeroDivisivelPor5 = (numero % 5) === 0; //o % é utilizado para pegar o resto da divisão
+const isNumeroDivisivelPor5 = (numero % 5) === 0; //o % é utilizado para pegar o resto da divisão
 
 //if é uma forma de criar condicionais para direcionar o que irá acontecer de acordo com a condição
 if (numero === 0) {
     console.log("Número inválido");
 
-} else if (numeroDivisivelPor5) { //usado para testar uma nova condição quando a anterior no if for falsa.
+} else if (isNumeroDivisivelPor5) { //usado para testar uma nova condição quando a anterior no if for falsa.
     console.log("Sim");
 
 } else { //bloco para se não entrar na condição do if cair nessa
