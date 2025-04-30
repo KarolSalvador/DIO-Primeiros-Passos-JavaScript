5c9931b82eb91035999563d1463304f7ffd6e8cb
KarolSalvador <karolsalvador91@gmail.com>
2025-04-30 17:12:32 -0300

    exercicio de tabuada usando repeticao

---
+++ 06 - arrays/tabuada.js
@@ -0,0 +1,10 @@
+// 1- Crie um programa que dado um número imprima a sua tabuada
+
+let numero = 2;
+
+for (let i = 1; i <= 10; i++) {
+    const tabuada = i * numero;
+    console.log(`${numero} * ${i} = ${tabuada}`);
+    
+}
+
